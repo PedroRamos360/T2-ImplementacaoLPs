15920d04586790994057e6ec96e184f2bc6fb356
Pedro Ramos <pedrowarkenramos@gmail.com>
2023-10-09 20:32:39 -0300

    first commit

---
+++ main.java
@@ -0,0 +1,15 @@
+// Coisas do gpt
+
+
+// Javassist example
+ClassPool pool = ClassPool.getDefault();
+CtClass newClass = pool.makeClass("MyNewClass");
+
+// Javassist example
+CtField newField = new CtField(CtClass.intType, "myField", newClass);
+newClass.addField(newField);
+
+
+// Javassist example
+CtMethod newMethod = CtNewMethod.make("public void myMethod() { /* method body */ }", newClass);
+newClass.addMethod(newMethod);
